651f301dc69e5e5a355b63d7a9d6b854ae1617e1
resouer <resouer@163.com>
2015-01-29 12:50:00 +0800

    Fix typo and bugs in README

--- cluster/ubuntu-cluster/configure.sh
+++ cluster/ubuntu-cluster/configure.sh
@@ -12,21 +12,21 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo "Welcome to use this script to configure k8s setup"
-read -p "Going to configure a master node press Y/y Or a minion node press N/n > " yn 
+read -p "Configure a master node press Y/y, configure a minion node press N/n > " yn 
 
 while true; do
 	case $yn in
 	    [Yy]* )
-            read -p "Enter your IP_address like 10.10.0.88 > " myIP
-            read -p "Enter your minion IP_address, comma separated like 10.10.0.34,10.10.0.35,10.10.0.36 > " minionIPs
+            read -p "Enter your k8s master IP address > " myIP
+            read -p "Enter your k8s minion IP addresses, comma separated like '<ip_1>,<ip_2>,<ip_3>' > " minionIPs
 	        # USE MY_IP as ETCD name 
 	        echo ETCD_OPTS=\"-name ${myIP} -addr ${myIP}:4001 -peer-addr ${myIP}:7001\" > default_scripts/etcd
 	        sed -i "s/MINION_IPS/${minionIPs}/g" default_scripts/kube-controller-manager        
 	        break
 	        ;;
 	    [Nn]* )
-            read -p "Enter your IP_address like 10.10.0.66 > " myIP
-            read -p "Enter the master node ip like 10.10.0.88 > " masterIP
+            read -p "Enter your k8s minion IP address > " myIP
+            read -p "Enter the k8s master node IP address > " masterIP
             # USE MY_IP as ETCD name
             echo ETCD_OPTS=\"-name ${myIP} -addr ${myIP}:4001 -peer-addr ${myIP}:7001 -peers=${myIP}:7001,${masterIP}:7001\" > default_scripts/etcd
 	        sed -i "s/MY_IP/${myIP}/g" default_scripts/kubelet
