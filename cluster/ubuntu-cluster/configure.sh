5d54083a2ea60e12ae4ff8ae1940ea5f5c0d4b49
wizard <wizardcxy@126.com>
2015-01-29 15:37:14 +0800

    change the shell script to pass ci again

--- cluster/ubuntu-cluster/configure.sh
+++ cluster/ubuntu-cluster/configure.sh
@@ -13,8 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# simple use the sed to replace some ip settings on user's demand
 
+# simple use the sed to replace some ip settings on user's demand
 # Run as root only
 
 set -e
